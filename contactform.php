270a72a4c50a30bea8c80d2f4831f22e21fa54f7
tonyarice <tonyacrice@gmail.com>
2021-10-23 23:27:58 -0400

    Added contact form using php

---
+++ contactform.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $subject = $_POST["subject"];
+    $mailFrom = $_POST["mail"];
+    $message = $_POST["message"];
+
+    $mailTo = "tonyaricecodes@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "Your have received an e-mail from ".$fname $lname.".\n\n".$message;
+
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Locations: index.php?mailsend");
+
+}
